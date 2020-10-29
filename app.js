6c565b0830868bf7a70b99375203a2ca6698920b
sschneeberg <simone.m.schneeberg@gmail.com>
2020-10-29 13:01:56 -0400

    [project] Initiate project

---
+++ app.js
@@ -0,0 +1,9 @@
+const car = {
+    make: 'VW Bug',
+    model: '?',
+    color: 'yellow',
+    start: function() { return 'Outta here...' },
+    stop: function() { return 'Hit the brakes !!' }
+}
+
+console.log(car.start());
